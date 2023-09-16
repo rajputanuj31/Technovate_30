21ced49b485ebc6943c8024e3c7cfd03c4a7d01f
rajputanuj31 <rajputanuj041@gmail.com>
2023-09-16 12:53:49 +0530

    Added MedicineNFT contract

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,4 +1,4 @@
-export default function Navbar () {
+export default function Contact () {
     return (
       <div className="flex min-h-screen flex-col items-center justify-between p-24">
           <h1 className="text-5xl text-green-600">Contact</h1>
---
+++ contracts/MedicineNFT.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts@4.9.3/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.9.3/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts@4.9.3/utils/Counters.sol";
+
+contract Medicine is ERC721, ERC721URIStorage {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("Saridon", "SDB") {}
+
+    function safeMint(address to, string memory uri) public {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
